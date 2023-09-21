085442a55e0f871aec5cf90067745e7224106033
Mumtaz Moon <37664171+mumtazhaqiqy@users.noreply.github.com>
2023-09-21 12:53:40 +0000

    commit app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image, ImageOps
+
+# Load the trained model
+model = tf.keras.models.load_model("mnist_model.h5")
+
+st.title("MNIST Digit Recognition")
+
+# Allow user to upload an image
+uploaded_file = st.file_uploader("Upload a drawn digit", type="png")
+
+if uploaded_file:
+    image = Image.open(uploaded_file).convert("L")
+    image = ImageOps.invert(image)
+    image = image.resize((28, 28))
+ä
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    # Preprocess the image and predict
+    image = np.array(image) / 255.0
+    image = image.reshape(1, 28, 28)
+    prediction = model.predict(image)
+
+    st.write(f"Predicted Digit: {np.argmax(prediction)}")
+    st.write(f"Confidence: {100 * np.max(prediction):.2f}%")
